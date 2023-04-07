077a3be24064b268d267a89864b26d0d8082328a
anbehindY <yanminthwin@gmail.com>
2023-04-07 14:48:52 +0630

    Change descriptions of the test file

--- src/modules/UnitTestPartTwo/editUpdateCompleteTask.test.js
+++ src/modules/UnitTestPartTwo/editUpdateCompleteTask.test.js
@@ -1,21 +1,21 @@
 const tasksEditUpdateCompleted = require('./editUpdateCompleteTask.js');
 const { tasksList, tasksAddRemove } = require('../UnitTestPartOne/addAndRemove.js');
 
-describe('test update todo list description by index number', () => {
+describe('Update todo list description with respective index number', () => {
   tasksAddRemove.addTask('task-1');
-  test('todo description as the index number in param should be updated', () => {
+  test('todo description should be updated with respective index number', () => {
     expect(tasksEditUpdateCompleted.editTaskDescription(0, 'edited')).toEqual(tasksList.description);
   });
 });
 
-describe('test update todo isCompleted by index number', () => {
-  test('todo isCompleted as the index number in param should be changes', () => {
+describe('Update todo isCompleted with respective index number', () => {
+  test('todo isCompleted is true as the index number in param should be changes', () => {
     expect(tasksEditUpdateCompleted.updateComplete(0)).toEqual(tasksList.isCompleted);
   });
 });
 
-describe('delete todo list were isCompleted status is true', () => {
-  test('todo list should only shown todo list with isCompleted status is false', () => {
+describe('delete todo list where isCompleted status is true', () => {
+  test('todo list should only show todo list with isCompleted status is false', () => {
     expect(tasksEditUpdateCompleted.removeAllCompleted()).toEqual(tasksList.length);
   });
 });
